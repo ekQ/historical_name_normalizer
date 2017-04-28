ca85b53813ab20eed5b474f93a266c847158a780
Eric Malmi <eric.malmi@aalto.fi>
2017-04-28 11:11:45 +0300

    added automatically inferred normalizations and improved debugging

--- name_normalizer.py
+++ name_normalizer.py
@@ -4,14 +4,19 @@ import codecs
 import re
 import time
 import jellyfish as jf
+import os
 
 # If a name is not found among the predefined mappings, it's mapped to the
 # nearest predefined name if the Jaro-Winkler similarity (0,1] is above this
 # threshold.
 SIMILARITY_THRESHOLD = 0.9
-# The list of available mappings.
-NAME_TYPES = ['first', 'last', 'patronym', 'cause_of_death']
+# The list of available mappings. The ones with suffix "_extended" include
+# automatically inferred mapping between name variations which help to improve
+# recall.
+NAME_TYPES = ['first', 'last', 'patronym', 'cause_of_death', 'last_extended',
+              'cause_of_death_extended']
 DEBUG = True
+WARNINGS = False
 
 
 def ensure_unicode(s):
@@ -27,26 +32,48 @@ def is_vowel(c):
     return c in u'aeiouyäö'
 
 
-def clean_name(token, clean_strong=1):
-    if token is None:
+def clean_name(name, name_type='first'):
+    '''
+    Cleans a name string by making some common substitutions, such as
+        c -> k
+    For certain name types ('last' and 'cause_of_death') additional custom
+    substitution rules are applied.
+
+    Return:
+        The cleaned name string.
+    '''
+    if name is None:
         return u""
-    token = token.lower().split('\k')[0].strip()
-    if clean_strong >= 1:
-        token = re.sub(u'[^a-zåäöé ]', '', token)
-        token = token.strip()
-        token = re.sub(u'c', u'k', token)
-        token = re.sub(u'å', u'o', token)
-        token = re.sub(u'w', u'v', token)
-        token = re.sub(u'é', u'e', token)
-    if clean_strong >= 2:
-        token = re.sub(u'io', u'jo', token)
-        token = re.sub(u'iö', u'jö', token)
-        token = re.sub(u'ph', u'ff', token)
-        if token.endswith(u'nen'):
-            token = token[:-2]
-        if token.endswith(u'in') and len(token) > 3 and is_vowel(token[-3]):
-            token = token[:-2] + u'n'
-    return token
+    name = name.lower().split('\k')[0].strip()
+    if name_type != 'dont_substitute':
+        name = re.sub(u'[^a-zåäöé ]', '', name)
+        name = name.strip()
+        name = re.sub(u'c', u'k', name)
+        name = re.sub(u'å', u'o', name)
+        name = re.sub(u'w', u'v', name)
+        name = re.sub(u'é', u'e', name)
+    if name_type.startswith('last'):
+        name = re.sub(u'io', u'jo', name)
+        name = re.sub(u'iö', u'jö', name)
+        name = re.sub(u'ph', u'ff', name)
+        if name.endswith(u'nen'):
+            name = name[:-2]
+        if name.endswith(u'in') and len(name) > 3 and is_vowel(name[-3]):
+            name = name[:-2] + u'n'
+    elif name_type.startswith('cause_of_death'):
+        name = re.sub(u'^af ', u'', name)
+        name = re.sub(u'^i ', u'', name)
+        name = re.sub(u'^död af ', u'', name)
+        name = re.sub(u'^död i ', u'', name)
+    return name
+
+
+def death_cause_post_cleaning(cause):
+    cause = re.sub(u'^af ', u'', cause)
+    cause = re.sub(u'^i ', u'', cause)
+    cause = re.sub(u'^död af ', u'', cause)
+    cause = re.sub(u'^död i ', u'', cause)
+    return cause
 
 
 class NameNormalizer:
@@ -57,36 +84,36 @@ class NameNormalizer:
                         name_type, ', '.join(NAME_TYPES)))
         self.name_type = name_type
         self.do_clean_names = do_clean_names
-        # The level of cleaning depends on name type.
-        self.clean_level = 1
-        if self.name_type == 'first':
-            self.clean_level = 1
-        elif self.name_type == 'last':
-            self.clean_level = 2
-        elif self.name_type == 'patronym':
-            self.clean_level = 1
-        elif self.name_type == 'cause_of_death':
-            self.clean_level = 1
         # Mapped names.
         self.name_map = self._load_name_map()
         self.name_trie = self._construct_trie()
 
-    def normalize(self, name, find_nearest=True):
+    def normalize(self, name, find_nearest=True, info=None):
         '''
         Returns a normalized form of the name.
 
         If find_nearest is True, goes through all predefined names and find the
         most similar one (if performance is an issue, set it to false).
+
+        If info dictionary is provided, some extra data about the call will be
+        stored there.
         '''
         t0 = time.time()
         name = u(name)
+        normalized = None
+        if info is None:
+            info = {}
+        success = False
         if self.do_clean_names:
-            name = clean_name(name, self.clean_level)
+            name = clean_name(name, self.name_type)
         if name in self.name_map:
             if DEBUG:
                 print u"Found exact match: {} -> {}".format(name, self.name_map[name][0])
-                print "Search took {:.4f} seconds.".format(time.time() - t0)
-            return self.name_map[name][0]
+            normalized = self.name_map[name][0]
+            success = True
+            info['nearest'] = name
+            info['sim'] = 1
+            info['norm_popularity'] = self.name_map[name][2]
         else:
             if not find_nearest:
                 match_node = self.name_trie.longest_common_prefix(name)
@@ -95,14 +122,17 @@ class NameNormalizer:
                     if DEBUG:
                         print u"Found approximate match: {} -> {} (sim={:.3f}) -> {}".format(
                                 name, match_node.top_name, sim, match_node.top_norm_name)
-                        print "Search took {:.4f} seconds.".format(time.time() - t0)
-                    return match_node.top_norm_name
+                    normalized = match_node.top_norm_name
+                    success = True
+                    info['norm_popularity'] = match_node.top_name_popularity
                 else:
                     if DEBUG:
                         print u"Only bad match found: {} -> {} (sim={:.3f}) -> {}".format(
                                 name, match_node.top_name, sim, match_node.top_norm_name)
-                        print "Search took {:.4f} seconds.".format(time.time() - t0)
-                    return name
+                    normalized = name
+                    info['norm_popularity'] = None
+                info['nearest'] = match_node.top_name
+                info['sim'] = sim
             else:
                 nearest = None
                 nearest_sim = -1
@@ -115,14 +145,21 @@ class NameNormalizer:
                     if DEBUG:
                         print u"Found approximate search match: {} -> {} (sim={:.3f}) -> {}".format(
                                 name, nearest, nearest_sim, self.name_map[nearest][0])
-                        print "Search took {:.4f} seconds.".format(time.time() - t0)
-                    return self.name_map[nearest][0]
+                    normalized = self.name_map[nearest][0]
+                    success = True
+                    info['norm_popularity'] = self.name_map[nearest][2]
                 else:
                     if DEBUG:
                         print u"Only bad search match found: {} -> {} (sim={:.3f}) -> {}".format(
                                 name, nearest, nearest_sim, self.name_map[nearest][0])
-                        print "Search took {:.4f} seconds.".format(time.time() - t0)
-                    return name
+                    normalized = name
+                    info['norm_popularity'] = None
+                info['nearest'] = nearest
+                info['sim'] = nearest_sim
+        if DEBUG:
+            print "Normalization took {:.3f} ms.".format(1000 * (time.time() - t0))
+        info['success'] = success
+        return normalized
 
     def normalize_all(self, name):
         '''
@@ -133,14 +170,19 @@ class NameNormalizer:
         return ' '.join(parts)
 
     def _load_name_map(self):
+        pd = os.path.dirname(os.path.abspath(__file__))  # Package directory.
         if self.name_type == 'first':
-            fname = 'data/normalized_first_names.tsv'
+            fname = os.path.join(pd, 'data', 'normalized_first_names.tsv')
         elif self.name_type == 'last':
-            fname = 'data/normalized_last_names.tsv'
+            fname = os.path.join(pd, 'data', 'normalized_last_names.tsv')
+        elif self.name_type == 'last_extended':
+            fname = os.path.join(pd, 'data', 'normalized_last_names_th0.90.tsv')
         elif self.name_type == 'patronym':
-            fname = 'data/normalized_patronyms.tsv'
+            fname = os.path.join(pd, 'data', 'normalized_patronyms.tsv')
         elif self.name_type == 'cause_of_death':
-            fname = 'data/normalized_death_causes.tsv'
+            fname = os.path.join(pd, 'data', 'normalized_death_causes.tsv')
+        elif self.name_type == 'cause_of_death_extended':
+            fname = os.path.join(pd, 'data', 'normalized_death_causes_th0.95.tsv')
         else:
             raise Exception('Unsupported name type.')
         f = codecs.open(fname, 'r', 'utf8')
@@ -150,18 +192,18 @@ class NameNormalizer:
             name = u(parts[0].lower())
             norm_name = u(parts[1].lower())
             if self.do_clean_names:
-                name = clean_name(name, self.clean_level)
+                name = clean_name(name, self.name_type)
             name_popularity = int(parts[2])
             name_group_popularity = int(parts[3])
             others = parts[4:]
             if name in name_map:
                 if name_map[name][0] != norm_name:
-                    print u'Incompatible mappings for {}:'.format(name)
-                    print name_map[name]
-                    print norm_name
-                    raise Exception('Incompatible mappings.')
+                    if WARNINGS:
+                        print u'  Ignoring mapping:\t{} -> {}'.format(name, norm_name)
+                        print u'  which contradicts a previous mapping:\t{} -> {}\n'.format(name, name_map[name][0])
+                    continue
                 else:
-                    if name_group_popularity != name_map[name][2]:
+                    if name_group_popularity != name_map[name][2] and WARNINGS:
                         print(u"Same name group but different popularities:"
                               u" {} ({}), {} ({})".format(
                                   name, name_group_popularity,
@@ -218,18 +260,18 @@ class TrieNode:
 
 if __name__ == "__main__":
     first_normalizer = NameNormalizer('first')
-    last_normalizer = NameNormalizer('last')
+    last_normalizer = NameNormalizer('last_extended')
     patronym_normalizer = NameNormalizer('patronym')
     cod_normalizer = NameNormalizer('cause_of_death')
 
     name = 'Hindrich'
-    print u"\nExample first name to normalize: {}\n".format(name)
+    print u"\nExample #1: first name to normalize: {}\n".format(name)
     normalized = first_normalizer.normalize(name)
     print u"Normalized: {}\n".format(normalized)
 
 
     name = u'Malmkvist'
-    print u"\nExample last name to normalize: {}\n".format(name)
+    print u"\nExample #2: last name to normalize: {}\n".format(name)
     normalized = last_normalizer.normalize(name, find_nearest=False)
     print u"Normalized based on prefix: {}\n".format(normalized)
     normalized = last_normalizer.normalize(name, find_nearest=True)
